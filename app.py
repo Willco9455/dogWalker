05aebf2de06373a7923b4bbc19f44f86295f3d3e
will <willmoran6969@gmail.com>
2021-02-04 12:53:35 +0000

    lesson3 v3

--- app.py
+++ app.py
@@ -2,16 +2,27 @@
     1. Flask - Flask is the object that creates the flask app and allows all the flask features to be used
     2. render_template - this import is used to allow HTML templates to be used 
     3. request = this import is used when paths are fist loaded to tell which method was used in loading the page
+    4. url_for = this is a flask term that is used to create urls that reference files/functions within the app
 '''
-from flask import Flask, render_template, request 
+from flask import Flask, render_template, request, url_for
 app = Flask(__name__)        ## Sets the app 
 
 ## This says that the function below will be run when the path "website/" is active the methods array is just flask 
 ## terminology that allows both different mehods to be used in that path(GET and POST)
 @app.route('/', methods=['GET', 'POST'])  
 def login():       
-    if request.method == "POST":
-        return "<h1>LOGIN SUCCESSFUL<h1>"
+    ## this will run when the submit button has been pressed becuase that makes the request method POST not GET
+    if request.method == "POST":  
+        usr = request.form["usr"] ## sets usr variable to the value that was in the username field
+        pas = request.form["pas"] ## sets pas variable to the value that was in the password field
+        ## this returns a temporary html page to display to test if the new functionality works
+        return f"""
+            <h1>LOGIN SUCCESSFUL</h1><br>
+            
+            <p>your username is {usr}</p>
+            <p>your password is {pas}</p>
+        """
+    ## This will run when first entereing the webpage 
     else: 
         return render_template("login.html")   ## Whats returned from the function is displayed on the browser so 
                                                ## LOGIN SCREEN will be displayed
@@ -19,3 +30,4 @@ def login():
 if __name__ == '__main__':  ## This makes sure the app runs when the python file is ran 
     app.run(debug = True)   ## The debug = true turns the debug on so that when there is an syntax error the 
                             ## browser will show where the problem is, making debugging eaiser
+
